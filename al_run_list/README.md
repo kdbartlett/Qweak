beecbe0bc5a084ee93b15d784097099bbf59baf4
kdbartlett <kurtisbartlett@gmail.com>
2017-02-07 16:52:36 -0500

    Working version of script.

--- al_run_list/README.md
+++ al_run_list/README.md
@@ -1,16 +1,19 @@
 #al_run_list
-Python script for extracting run information from the Qweak mysql db into a csv file from future use.
+Python script for extracting run information from the Qweak mysql db into a csv file for future use.
 
 ##Requirements
 The script requires the following python packages for operation: 
 
 * python3
 * PyMySQL
-* PyYAML 
+* PyYAML
+* Pandas
+* Numpy
 
 ##Usage
 Pass the get_run_list.py executable the following parameters:
 
 ```bash
-./get_run_list.py
+pytnon3 get_run_list.py <run#> <file.csv>
 ```
+where <run#> is either `run1` or `run2` and file.csv is a .csv file of your choosing.
--- al_run_list/get_run_list.py
+++ al_run_list/get_run_list.py
@@ -2,6 +2,8 @@
 # Kurtis Bartlett
 # 2017/2/7
 #
+# Created with help from Wade.
+#
 #!/usr/bin/env python3
 #===============================================================================
 
@@ -11,5 +13,40 @@ import pymysql
 import yaml
 import csv
 import sys
+import pandas as pd
+import numpy as np
 
+def connectMySQL(run_num):
+    '''Read in yaml configuration file and return a connection using the data from the file.'''
+
+    #Open connection and read it into directory
+    mysql_db = None
+    with open('qweak_db_%s_read_cred.yml'%(run_num)) as f:
+        mysql_db = yaml.safe_load(f)
+        if mysql_db == None:
+            print('Cannot open qweak_db_run1_read_cred.yml, is the config correct?')
+    return pymysql.connect(**mysql_db)
+
+if __name__ == '__main__':
+
+    #Open connection to database and create a cursor to pass around
+    try:
+        connection = connectMySQL(sys.argv[1])
+    except pymysql.err.OperationalError:
+        print('Failed to open connection to the database!')
+    else:
+        cursor = connection.cursor()
+        
+        sql = "SELECT bm.run_number AS 'Run #', bm.slug AS 'Slug', bm.wien_slug AS 'Wien', sc.target_position AS 'Target', ROUND(MAX(bm.value),0) AS 'Beam Current', ROUND(MAX(sc.qtor_current),0) AS 'QTor Current', sc.slow_helicity_plate AS 'IHWP1', sc.passive_helicity_plate AS 'IHWP2' FROM beam_view AS bm LEFT JOIN slow_controls_settings AS sc ON bm.runlet_id=sc.runlet_id WHERE bm.monitor='qwk_bcm8' AND bm.slug>=1000 AND bm.slug<2000 AND bm.value > 0 GROUP BY bm.run_number"
+        
+
+        cursor.execute(sql)
+        sql_result = cursor.fetchall()
+        data = pd.DataFrame(np.asarray(sql_result))
+        data.to_csv(sys.argv[2], index=False, header=False)
+        #print(sql_result)
+
+        #Close connections
+        cursor.close()
+        connection.close
 
