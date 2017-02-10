3075ccc722f9273a60ef6b7aba2403adf8ea4668
kdbartlett <kurtisbartlett@gmail.com>
2017-02-10 15:49:35 -0500

    Cleaned up over-run lines, included argparse for improved input handling.

--- al_run_list/get_run_list.py
+++ al_run_list/get_run_list.py
@@ -1,14 +1,16 @@
+#!/usr/bin/env python3
 # ==============================================================================
 # Kurtis Bartlett
 # 2017/2/7
 #
-# Created with help from Wade.
+# Python script for connecting to Qweak SQL database, passing desired query,
+# returning all rows from query in a .csv file. Created with help from Wade.
 #
-# !/usr/bin/env python3
 # ==============================================================================
 
 
-# Import necessary libraries/packages
+# Import necessary packages/modules
+import argparse
 import pymysql
 import yaml
 import csv
@@ -19,33 +21,55 @@ import numpy as np
 
 # Function for connecting to sql db.
 def connect_mysql(run_num):
-    '''Read in yaml configuration file and return a connection using the data from the file.'''
+    """
+    Read in yaml configuration file,
+    then return a connection using the data from the file.
+    """
 
     # Open connection and read it into directory
     with open('qweak_db_%s_read_cred.yml' % (run_num)) as f:
         mysql_db = yaml.safe_load(f)
         if mysql_db is None:
-            raise Exception('Cannot open qweak_db_run#_read_cred.yml, config correct?')
+            raise Exception('Cannot open qweak_db_run#_read_cred.yml!')
     return pymysql.connect(**mysql_db)
 
 
 # Temp function for sql query of interest
 def sql_query(target):
-    sql = "SELECT bm.run_number AS 'Run #', bm.slug AS 'Slug', bm.wien_slug AS 'Wien', sc.target_position AS 'Target', ROUND(MAX(bm.value),0) AS 'Beam Current', ROUND(MAX(sc.qtor_current),0) AS 'QTor Current', sc.wien_reversal AS 'Pol. State' ,sc.slow_helicity_plate AS 'IHWP1', sc.passive_helicity_plate AS 'IHWP2' FROM beam_view AS bm LEFT JOIN slow_controls_settings AS sc ON bm.runlet_id=sc.runlet_id WHERE sc.target_position='%s' AND bm.monitor='qwk_bcm6' AND bm.value > 0 GROUP BY bm.run_number" % (target)
+    """Sql query of interest for the qweakdb"""
+
+    sql = """\
+    SELECT bm.run_number, bm.slug, bm.wien_slug, sc.target_position,
+    ROUND(MAX(bm.value),0), ROUND(MAX(sc.qtor_current),0), sc.wien_reversal,
+    sc.slow_helicity_plate, sc.passive_helicity_plate
+    FROM beam_view AS bm LEFT JOIN slow_controls_settings AS sc
+    ON bm.runlet_id=sc.runlet_id
+    WHERE sc.target_position='%s'
+    AND bm.monitor='qwk_bcm6'
+    AND bm.value > 0
+    GROUP BY bm.run_number""" % (target)
     return sql
 
 if __name__ == '__main__':
+    # Allow parsing of inputs.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('run_num', type=str, help='Enter run1 or run2.')
+    parser.add_argument('target_type', type=str,
+                        help='Enter valid qweakdb target type string.')
+    parser.add_argument('file_name', type=str,
+                        help='Enter name of .csv file.')
+    inputs = parser.parse_args()
 
     # Open connection to database and create a cursor to pass around
     try:
-        connection = connect_mysql(sys.argv[1])
+        connection = connect_mysql(inputs.run_num)
     except pymysql.err.OperationalError:
         print('Failed to open connection to the database!')
     cursor = connection.cursor()
 
     # Pass the sql query function the slug range of interest
-    query = sql_query(sys.argv[2])
-    # print(query)
+    query = sql_query(inputs.target_type)
+
     # Execute query and collect all rows.
     cursor.execute(query)
     sql_result = cursor.fetchall()
@@ -53,8 +77,7 @@ if __name__ == '__main__':
     # Fetchall returns list of tuples,
     # throw into Pandas dataframe for quick export to csv.
     data = pd.DataFrame(np.asarray(sql_result))
-    data.to_csv(sys.argv[3], index=False, header=False)
-    # print(query)
+    data.to_csv(inputs.file_name, index=False, header=False)
 
     # Close connections
     cursor.close()
