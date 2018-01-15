3797ec7efbc89fc925ceb6e1528fa3eedc24c951
kdbartlett <kurtisbartlett@gmail.com>
2018-01-15 09:49:30 -0500

    DB runlist script mods.

--- al_run_list/get_run_list.py
+++ al_run_list/get_run_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # ==============================================================================
 # Kurtis Bartlett
-# 2017/2/7
+# 2017/12/6
 #
 # Python script for connecting to Qweak SQL database, passing desired query,
 # returning all rows from query in a .csv file. Created with help from Wade.
@@ -34,30 +34,14 @@ def connect_mysql(run_num):
     return pymysql.connect(**mysql_db)
 
 
-# Temp function for sql query of interest
-def sql_query(target):
-    """Sql query of interest for the qweakdb"""
-
-    sql = """\
-    SELECT bm.run_number, bm.slug, bm.wien_slug, sc.target_position,
-    ROUND(MAX(bm.value),0), ROUND(MAX(sc.qtor_current),0), sc.wien_reversal,
-    sc.slow_helicity_plate, sc.passive_helicity_plate
-    FROM beam_view AS bm LEFT JOIN slow_controls_settings AS sc
-    ON bm.runlet_id=sc.runlet_id
-    WHERE sc.target_position='%s'
-    AND bm.monitor='qwk_bcm6'
-    AND bm.value > 0
-    GROUP BY bm.run_number""" % (target)
-    return sql
-
 if __name__ == '__main__':
     # Allow parsing of inputs.
     parser = argparse.ArgumentParser()
     parser.add_argument('run_num', type=str, help='Enter run1 or run2.')
-    parser.add_argument('target_type', type=str,
-                        help='Enter valid qweakdb target type string.')
+    parser.add_argument('query', type=str,
+                        help='Enter valid sql query file.')
     parser.add_argument('file_name', type=str,
-                        help='Enter name of .csv file.')
+                        help='Enter name of output .csv file.')
     inputs = parser.parse_args()
 
     # Open connection to database and create a cursor to pass around
@@ -67,8 +51,10 @@ if __name__ == '__main__':
         print('Failed to open connection to the database!')
     cursor = connection.cursor()
 
-    # Pass the sql query function the slug range of interest
-    query = sql_query(inputs.target_type)
+    # Open .sql file
+    query_file = open(inputs.query, 'r')
+    query = query_file.read()
+    query_file.close()
 
     # Execute query and collect all rows.
     cursor.execute(query)
